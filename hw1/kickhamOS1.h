f84b7bba35c4ac550fa206f2016648062eaf897d
tkicks <tkicks@aten.(none)>
2015-09-18 18:04:59 -0400

    starting off

---
+++ hw1/kickhamOS1.h
@@ -0,0 +1,5 @@
+#include <string>
+using namespace std;
+
+void readFile(string filename, int* numList[]);		// definition of readFile
+void sortList(int* numList[], int numLines);		// definition of sortList
