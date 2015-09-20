c056c945de407cb6de5d44a551c6b6d56b7a567b
Tyler <tkicks32@gmail.com>
2015-09-20 17:46:08 -0400

    sync

--- hw1/kickhamOS1.h
+++
@@ -1,5 +0,0 @@
-#include <string>
-using namespace std;
-
-void readFile(string filename, int* numList[]);		// definition of readFile
-void sortList(int* numList[], int numLines);		// definition of sortList
